11b2f83613ae6a6bfcaba50637b3e4187e05cc0a
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-23 22:28:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end  
+
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} nad I am learning to program in #{language}."
+end
